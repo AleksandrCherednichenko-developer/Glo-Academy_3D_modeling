cbb1a3743fc5b25f050ad12a3dded3e1b365ffb4
AlexanderCherednicheenko <acherednichenko174@gmail.com>
2021-08-22 22:20:11 +0300

    выполнение усложненного задания в script.js

--- js/script.js
+++ js/script.js
@@ -59,6 +59,17 @@ window.addEventListener('DOMContentLoaded', function () {
          elem.addEventListener('click', handlerMenu);
       });
 
+      const anchors = document.querySelectorAll('a[href*="#"]')
+      for (let anchor of anchors) {
+      anchor.addEventListener('click', function (e) {
+         e.preventDefault();
+         const blockID = anchor.getAttribute('href').substr(1);
+         document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         });
+      });
+      }
    }
    toggleMenu();
 
